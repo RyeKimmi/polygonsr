375b7d5c2fcc9aa9ab1844687acd594fb0b52ccb
Ryan Kim <skim19@stuy.edu>
2019-04-02 21:42:30 -0400

    wrote code for calculating normal of triangle

--- gmath.py
+++ gmath.py
@@ -12,11 +12,20 @@ def magnitude(vector):
 #normalize vetcor, should modify the parameter
 def normalize(vector):
     m = magnitude(vector)
-    return [vector[0]/m, vector[1]/m, vector[2]/m]
+    vector[0] = vector[0]/m
+    vector[1] = vector[1]/m
+    vector[2] = vector[2]/m
+    pass
 
 #Return the dot product of a . b
 def dot_product(a, b):
     return a[0]*b[0] + a[1]*b[1] + a[2]*b[2] 
+
+#Return the cross product of a x b
+def cross_product(a,b):
+    return [a[1]*b[2] - a[2]*b[1],
+            a[2]*b[0] - a[0]*b[2],
+            a[0]*b[1] - a[1]*b[0]]
 
 #find angle of a and b
 def ret_angle(a, b):
@@ -28,5 +37,10 @@ def ret_angle(a, b):
 #Calculate the surface normal for the triangle whose first
 #point is located at index i in polygons
 def calculate_normal(polygons, i):
-    a = polygons[i]
-    return None
+    a = [ polygons[i+1][0] - polygons[i][0],
+          polygons[i+1][1] - polygons[i][1],
+          polygons[i+1][2] - polygons[i][2] ]
+    b = [ polygons[i+2][0] - polygons[i][0],
+          polygons[i+2][1] - polygons[i][1],
+          polygons[i+2][2] - polygons[i][2] ]
+    return cross_product(a,b)
